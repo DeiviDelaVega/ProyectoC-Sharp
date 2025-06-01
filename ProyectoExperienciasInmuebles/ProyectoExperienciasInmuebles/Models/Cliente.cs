5ecb429dbc907af30c32f8c89429d7a18fa20805
Allinzon <allinzonhuaicha@gmail.com>
2025-06-01 10:45:09 -0500

    MantClientes
    Implementación de mantenimiento de los clientes

--- ProyectoExperienciasInmuebles/ProyectoExperienciasInmuebles/Models/Cliente.cs
+++ ProyectoExperienciasInmuebles/ProyectoExperienciasInmuebles/Models/Cliente.cs
@@ -8,17 +8,28 @@ namespace ProyectoExperienciasInmuebles.Models
 {
     public class Cliente
     {
+        [Display(Name = "Id Cliente")]
+        public int IdCliente { get; set; }
+
         [Display(Name = "Nombre"), Required]
         public string nombre { get; set; }
+
         [Display(Name = "Apellido"), Required]
         public string apellido { get; set; }
+
         [Display(Name = "Documento"), Required]
         public string nroDocumento { get; set; }
+
         [Display(Name = "Direccion"), Required]
         public string direccion {  get; set; }
+
         [Display(Name = "Telefono"), Required]
         public string telefono { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha Registro"), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime fechaRegistro { get; set; }
+
         [Display(Name = "Correo"), Required]
         public string correo {  get; set; }
 
