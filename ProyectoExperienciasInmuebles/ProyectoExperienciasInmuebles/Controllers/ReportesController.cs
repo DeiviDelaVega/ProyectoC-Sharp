8dcce828d069526d1f17203e2e10c6a415ec188f
Allinzon <allinzonhuaicha@gmail.com>
2025-06-25 18:25:24 -0500

    ReporteClientesConMasReservas

---
+++ ProyectoExperienciasInmuebles/ProyectoExperienciasInmuebles/Controllers/ReportesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoExperienciasInmuebles.Models;
+
+namespace ProyectoExperienciasInmuebles.Controllers
+{
+    public class ReportesController : Controller
+    {
+
+        DAOReportes db = new DAOReportes();
+
+        public ActionResult ClientesConMasReservas()
+        {
+            var datos = db.ObtenerTop5ClientesConMasReservas();
+            return View(datos);
+        }
+    }
+}
---
+++ ProyectoExperienciasInmuebles/ProyectoExperienciasInmuebles/Models/DAOReportes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoExperienciasInmuebles.Models
+{
+    public class DAOReportes
+    {
+        public SqlConnection cn = new SqlConnection(
+            ConfigurationManager.ConnectionStrings["ExperienciasInmueble"].ConnectionString);
+
+        public List<ClientesMasReservas> ObtenerTop5ClientesConMasReservas()
+        {
+            List<ClientesMasReservas> lista = new List<ClientesMasReservas>();
+            SqlCommand cmd = new SqlCommand("usp_clientesMasReservas", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                cn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    ClientesMasReservas obj = new ClientesMasReservas()
+                    {
+                        Cliente = dr["Cliente"].ToString(),
+                        CantReservas = Convert.ToInt32(dr["CantReservas"])
+                    };
+                    lista.Add(obj);
+                }
+                dr.Close();
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return lista;
+        }
+    }
+}
